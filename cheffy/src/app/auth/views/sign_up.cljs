ae96c16068322dbf1d4e75755c06c10a2e113bda
David Mejorado <david.mejorado@gmail.com>
2021-11-30 19:49:56 -0800

    17 - sign-up

--- cheffy/src/app/auth/views/sign_up.cljs
+++ cheffy/src/app/auth/views/sign_up.cljs
@@ -1,7 +1,58 @@
-(ns app.auth.views.sign-up 
+(ns app.auth.views.sign-up
   (:require
-    [app.components.page-nav :refer [page-nav]]))
+   [app.components.mui :refer [button container grid text-field]]
+   [app.components.page-nav :refer [page-nav]]
+   [re-frame.core :as rf]
+   [reagent.core :as r]))
 
 (defn sign-up
   []
-  [page-nav {:center "Sign up"}])
+  (let [initial-values {:first-name ""
+                        :last-name ""
+                        :email ""
+                        :password ""}
+        values (r/atom initial-values)]
+    (fn []
+      [:<>
+       [page-nav {:center "Sign up"}]
+       [container {:max-width "sm"}
+        [:form {:on-submit (fn [evt]
+                             (.preventDefault evt)
+                             (rf/dispatch [:sign-up @values]))}
+         [grid {:container true}
+          [grid {:item true :xs 12 :pb 2}
+           [text-field {:label "First name"
+                        :type "text"
+                        :value (:first-name @values)
+                        :on-change #(swap! values assoc :first-name (.. % -target -value))
+                        :required true
+                        :full-width true}]]
+           [grid {:item true :xs 12 :pb 2}
+            [text-field {:label "Last name"
+                         :type "text"
+                         :value (:last-name @values)
+                         :on-change #(swap! values assoc :last-name (.. % -target -value))
+                         :required true
+                         :full-width true}]]
+           [grid {:item true :xs 12 :pb 2}
+            [text-field {:label "Email"
+                         :type "email"
+                         :value (:email @values)
+                         :on-change #(swap! values assoc :email (.. % -target -value))
+                         :required true
+                         :full-width true}]]
+          [grid {:item true :xs 12 :pb 2}
+           [text-field {:label "Password"
+                        :type "password"
+                        :value (:password @values)
+                        :on-change #(swap! values assoc :password (.. % -target -value))
+                        :required true
+                        :full-width true}]]
+          [grid {:item true
+                 :xs 12
+                 :display "flex"
+                 :justify-content "space-between"}
+           [:a {:href "#log-in"
+                :on-click #(rf/dispatch [:set-active-nav :log-in])}
+            "Already have an account? Log in!"]
+           [button {:type "submit"} "Sign up"]]]]]])))
