baa9a16edfab1cb69ff8015bd4554fad8e9e8a93
David Mejorado <david.mejorado@gmail.com>
2021-12-19 15:21:06 -0800

    18 - profile

--- cheffy/src/app/auth/views/profile.cljs
+++ cheffy/src/app/auth/views/profile.cljs
@@ -1,6 +1,51 @@
 (ns app.auth.views.profile
-  (:require [app.components.page-nav :refer [page-nav]]))
+  (:require
+   [app.components.mui :refer [box button container paper text-field
+                               typography]]
+   [app.components.page-nav :refer [page-nav]]
+   [re-frame.core :as rf]
+   [reagent.core :as r]))
 
 (defn profile
   []
-  [page-nav {:center "Profile"}])
+  (let [{:keys [first-name last-name]} @(rf/subscribe [:active-user-profile])
+        initial-values {:first-name first-name :last-name last-name}
+        values (r/atom initial-values)]
+    (fn []
+      [:<>
+       [page-nav {:center :Profile
+                  :right [button {:variant :outlined
+                                  :on-click #(rf/dispatch [:log-out])} "Log out"]
+                  }]
+       [container {:max-width :sm}
+        [paper {:elevation 1 :sx {:p 2}}
+         [typography {:variant :h6} "Personal Info"]
+         [:form {:on-click (fn [evt]
+                             (.preventDefault evt)
+                             (rf/dispatch [:update-profile @values]))}
+          [box {:display :flex :flex-direction :column}
+           [text-field {:label "First name"
+                        :type :text
+                        :value (:first-name @values)
+                        :on-change #(swap! values assoc :first-name (.. % -target -value))
+                        :required true
+                        :full-width true
+                        :sx {:my 1}}]
+           [text-field {:label "Last name"
+                        :type :text
+                        :value (:last-name @values)
+                        :on-change #(swap! values assoc :last-name (.. % -target -value))
+                        :required true
+                        :full-width true
+                        :sx {:my 1}}]
+           [button {:variant :contained
+                    :type :submit
+                    :sx {:align-self :end}} "Save"]]]]
+
+        [paper {:elevation 1 :sx {:p 2 :mt 2}}
+         [typography {:variant :h6} "Danger zone"]
+         [button {:variant :contained
+                  :color :error
+                  :on-click #(when (js/confirm "This will delete your account")
+                               (rf/dispatch [:delete-account]))}
+          "Delete Account"]]]])))
--- cheffy/src/app/components/mui.cljs
+++ cheffy/src/app/components/mui.cljs
@@ -5,6 +5,7 @@
             ["@mui/material/Container" :default MuiContainer]
             ["@mui/material/Grid" :default MuiGrid]
             ["@mui/material/FormGroup" :default MuiFormGroup]
+            ["@mui/material/Paper" :default MuiPaper]
             ["@mui/material/TextField" :default MuiTextField]
             ["@mui/material/Tab" :default MuiTab]
             ["@mui/material/Tabs" :default MuiTabs]
@@ -15,6 +16,7 @@
 (def box (r/adapt-react-class MuiBox))
 (def container (r/adapt-react-class MuiContainer))
 (def form-group (r/adapt-react-class MuiFormGroup))
+(def paper (r/adapt-react-class MuiPaper))
 (def grid (r/adapt-react-class MuiGrid))
 (def tab (r/adapt-react-class MuiTab))
 (def tabs (r/adapt-react-class MuiTabs))
